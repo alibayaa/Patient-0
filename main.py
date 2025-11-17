8bfcd76db4c327366a5805bc82272d4c3560f991
Ali <alibayaa07@gmail.com>
2025-11-17 15:53:56 -0500

    Basic game window set up and created test surface

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 600))
+pygame.display.set_caption('Patient 0')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Red')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    screen.blit(test_surface, (0, 0))
+
+    pygame.display.update()
+    clock.tick(60)
