25916f6284265a9b4ed6308410b093ca23ce10e0
Ali <alibayaa07@gmail.com>
2025-11-27 12:48:48 -0500

    Making progress, jump added

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ scaled_bg = pygame.transform.scale(background_surface, (1000, 600))
 scaled_floor = pygame.transform.scale(ground_surface, (1000, 100))
 
 text_font = pygame.font.Font('graphics/Pixeltype.ttf', 40)
-text_surface = text_font.render('My game', False, 'darkslategray4')
+score_surface = text_font.render('Score:', False, 'darkslategray4')
+score_rect = score_surface.get_rect(center = (500, 50))
 
 slime1_surface = pygame.image.load('graphics/slime/slime1.png').convert_alpha()
 scaled_slime1 = pygame.transform.scale(slime1_surface, (175, 50))
@@ -23,23 +24,47 @@ slime_rect = scaled_slime1.get_rect(bottomright = (1000, 500))
 player_surface = pygame.image.load('graphics/player/playeridle.png').convert_alpha()
 scaled_playeridle = pygame.transform.scale(player_surface, (250, 125))
 player_rect = scaled_playeridle.get_rect(midbottom = (100, 500))
+player_gravity = 0
+
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
+       
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if player_rect.bottom == 500:
+                    player_gravity = -20
+
     screen.blit(scaled_bg, (0, 0))
     screen.blit(scaled_floor, (0, 500))
-    screen.blit(text_surface, (0, 0))
     
+    # Score
+
+    pygame.draw.rect(screen, "darkslategray3", score_rect)
+    pygame.draw.rect(screen, 'darkslategrey', score_rect, 2)
+    screen.blit(score_surface, (score_rect))
+    
+    # Slime
+
     screen.blit(scaled_slime1, slime_rect)
     slime_rect.x -= 4
+    if slime_rect.right <= 0: slime_rect.left = 1000
+    
+    # Player
 
-    player_rect.left += 1
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 500: player_rect.bottom = 500
     screen.blit(scaled_playeridle, player_rect)
 
+    # Collisions
+    if slime_rect.colliderect(player_rect):
+        pygame.quit()
+        sys.exit()
 
     pygame.display.update()
     clock.tick(60)
